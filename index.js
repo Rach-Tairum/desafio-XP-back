1d0d19c28a8917813e69c22f540ca87b0f518450
Rachel Tairum <rachel.tairum@gmail.com>
2022-07-18 11:26:43 -0300

    fix: insere cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require ('express');
 require('dotenv/config');
+const cors = require('cors')
 
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 
 const port = process.env.PORT || 3000
 
